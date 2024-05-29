9f658d660adc37136da21dba7cc0ead6a7e7b3c3
AndradeRevillaErick <erick_andradhe@hotmail.com>
2024-05-29 07:25:54 -0600

    revert this one

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -18,6 +18,18 @@ const routes: Routes = [
     component: UncommonPageComponent,
   },
   {
+    path: 'uncommon',
+    component: UncommonPageComponent,
+  },
+  {
+    path: 'uncommon',
+    component: UncommonPageComponent,
+  },
+  {
+    path: 'uncommon',
+    component: UncommonPageComponent,
+  },
+  {
     path: '**',
     redirectTo: '',
   },
